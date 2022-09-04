68a1feab7ed76d159b18506fc1fa0c6fd7ac41f3
Moelaal <medo.elaal@gmail.com>
2022-09-04 05:41:36 +0200

    random cat

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-// import '../assets/css/style.css';
+import '../assets/css/style.css';
 import '../assets/css/mdb.min.css';
 
 const mainBtn = document.querySelector('#click-me');
 const resetBtn = document.querySelector('#reset');
 const daysSpan = document.querySelector('.days');
-console.log(typeof birthYear);
 
 function calcAge(age) {
   const now = new Date();
@@ -20,4 +19,25 @@ mainBtn.addEventListener('click', () => {
 
 resetBtn.addEventListener('click', () => {
   daysSpan.innerHTML = '';
+});
+//////////////////////
+
+// const CAT_URL = 'https://aws.random.cat/meow';
+
+const catBtn = document.querySelector('.btn-cat');
+const main = document.getElementById('main-div');
+// const catContainer = document.querySelector('.cat-cont');
+// const catimg = document.querySelector('#cat');
+catBtn.addEventListener('click', async () => {
+  // await fetch(CAT_URL)
+  //   .then((res) => res.json())
+  //   .then((res) => {
+  //     catimg.src = res.file;
+  //   });
+  const div = document.createElement('div');
+  div.className = 'col-sm-3 m-1';
+  const img = document.createElement('img');
+  img.src = 'https://thecatapi.com/api/images/get?format=src&type=gif';
+  div.appendChild(img);
+  main.appendChild(div);
 });
